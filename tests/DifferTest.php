db37f1cb61da273999c48496bf9ab4c53892def0
Alexander Shcherbachenko <ascherbachenko@cs-cart.com>
2018-11-18 11:37:54 +0400

    More unit test added

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -10,26 +10,67 @@ class DifferTest extends TestCase
 {
     public function testGeneratePrettyDiffForJsonFiles()
     {
-        $diff = getDiff(__DIR__ . '/fixtures/json/before.json', __DIR__ . '/fixtures/json/after.json', 'pretty');
-        $this->assertStringEqualsFile(__DIR__ . '/fixtures/json/expected_pretty.diff', $diff);
+        $diff = getDiff(
+            __DIR__ . '/fixtures/json/before.json',
+            __DIR__ . '/fixtures/json/after.json',
+            'pretty'
+        );
+        $this->assertStringEqualsFile(__DIR__ . '/fixtures/diff/expected_pretty.diff', $diff);
 
-        $diff = getDiff(__DIR__ . '/fixtures/json/nested_before.json', __DIR__ . '/fixtures/json/nested_after.json');
-        $this->assertStringEqualsFile(__DIR__ . '/fixtures/json/nested_expected_pretty.diff', $diff);
+        $nestedDiff = getDiff(
+            __DIR__ . '/fixtures/json/nested_before.json',
+            __DIR__ . '/fixtures/json/nested_after.json'
+        );
+        $this->assertStringEqualsFile(__DIR__ . '/fixtures/diff/nested_expected_pretty.diff', $nestedDiff);
     }
 
-    public function testGeneratePlainDiffFromJsonFiles()
+    public function testGeneratePlainDiffForJsonFiles()
     {
         $diff = getDiff(
+            __DIR__ . '/fixtures/json/before.json',
+            __DIR__ . '/fixtures/json/after.json',
+            'plain'
+        );
+        $this->assertStringEqualsFile(__DIR__ . '/fixtures/diff/expected_plain.diff', $diff);
+
+        $nestedDiff = getDiff(
             __DIR__ . '/fixtures/json/nested_before.json',
             __DIR__ . '/fixtures/json/nested_after.json',
             'plain'
         );
-        $this->assertStringEqualsFile(__DIR__ . '/fixtures/json/nested_expected_plain.diff', $diff);
+        $this->assertStringEqualsFile(__DIR__ . '/fixtures/diff/nested_expected_plain.diff', $nestedDiff);
     }
 
-    public function testGenerateDiffForYamlFiles()
+    public function testGeneratePrettyDiffForYamlFiles()
     {
-        $diff = getDiff(__DIR__ . '/fixtures/yaml/before.yaml', __DIR__ . '/fixtures/yaml/after.yaml');
-        $this->assertStringEqualsFile(__DIR__ . '/fixtures/yaml/expected.diff', $diff);
+        $diff = getDiff(
+            __DIR__ . '/fixtures/yaml/before.yaml',
+            __DIR__ . '/fixtures/yaml/after.yaml'
+        );
+        $this->assertStringEqualsFile(__DIR__ . '/fixtures/diff/expected_pretty.diff', $diff);
+
+        $nestedDiff = getDiff(
+            __DIR__ . '/fixtures/yaml/nested_before.yaml',
+            __DIR__ . '/fixtures/yaml/nested_after.yaml',
+            'pretty'
+        );
+        $this->assertStringEqualsFile(__DIR__ . '/fixtures/diff/nested_expected_pretty.diff', $nestedDiff);
+    }
+
+    public function testGeneratePlainDiffForYamlFiles()
+    {
+        $diff = getDiff(
+            __DIR__ . '/fixtures/yaml/before.yaml',
+            __DIR__ . '/fixtures/yaml/after.yaml',
+            'plain'
+        );
+        $this->assertStringEqualsFile(__DIR__ . '/fixtures/diff/expected_plain.diff', $diff);
+
+        $nestedDiff = getDiff(
+            __DIR__ . '/fixtures/yaml/nested_before.yaml',
+            __DIR__ . '/fixtures/yaml/nested_after.yaml',
+            'plain'
+        );
+        $this->assertStringEqualsFile(__DIR__ . '/fixtures/diff/nested_expected_plain.diff', $nestedDiff);
     }
 }
